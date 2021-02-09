ca17a4459a543a6b4a3c499087611a0d4924a1f4
MarioBrossBlack <crossholly4872@gmail.com>
2021-02-09 07:37:39 +0700

    form gaji

---
+++ form-gaji.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gaji Karyawan</title>
+</head>
+<body>
+    <form action="gaji.php" method="POST">
+     <table>
+      <tr>
+       <td>NIP :</td>
+       <td>
+        <input type="text" name="nip">
+       </td>
+      </tr>
+      <tr>
+       <td>Golongan :</td>
+       <td></td>
+      </tr>
+     </table>
+    </form>
+</body>
+</html>
